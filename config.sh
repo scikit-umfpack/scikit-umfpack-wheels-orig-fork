e4395f2b29a9a8e5a8b2a904adb9324317d183d3
xoviat <xoviat@users.noreply.github.com>
2017-10-11 09:58:22 -0500

    TST: use nose

--- config.sh
+++ config.sh
@@ -66,10 +66,6 @@ function run_tests {
     else
         local testmode="fast"
     fi
-    # Check bundled license file
-    python ../check_license.py
-    # Run tests
-    python ../run_scipy_tests.py $testmode -- -n2 -rfEX
-    # Show BLAS / LAPACK used
-    python -c 'import scipy; scipy.show_config()'
+    # Run nose tests
+    python -c 'nosetests -v scikits.umfpack'
 }
